3563160a54c1c3c4b41c345261763eb1e3ec8c98
raphaelpg <raphael_pinto@outlook.com>
2019-10-16 10:08:11 +0200

    exo_1.1.1

---
+++ Exercice_1.1.1.js
@@ -0,0 +1,19 @@
+const readline = require('readline')
+const rli = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let solution = Math.floor(Math.random()*100+1)
+console.log(solution)
+console.log("Tentez de trouver le nombre entre 0 et 100:")
+rli.on('line', (userinput) => {
+    console.log("Vous avez entré:", userinput)
+    if (Math.abs(userinput - solution) <= 10 && userinput != solution) {
+        console.log("vous n'êtes pas loin")
+    } else if (userinput == solution){
+        console.log("Bravo vous avez trouvé")
+        rli.close()
+    } else {
+        console.log("Vous êtes loin d'avoir trouvé la solution")
+    }
+})
