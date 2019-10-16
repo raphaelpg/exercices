e85f6ff4d62e3de5f56dfb5991caa7e2f1c8c0d2
raphaelpg <raphael_pinto@outlook.com>
2019-10-16 14:38:20 +0200

    exo_1.1.4

---
+++ Exercice_1.1.4.js
@@ -0,0 +1,12 @@
+class Cercle {
+    constructor(rayon) {
+        this.rayon = rayon;
+    }
+    aire() {
+        return (this.rayon * Math.PI).toFixed(2)
+    }
+    perimetre() {
+        return (this.rayon * 2 * Math.PI).toFixed(2)
+    }
+  }
+  
