530e4194e67144a4fcc80c863ad449f418f11b24
raphaelpg <raphael_pinto@outlook.com>
2019-11-08 12:49:46 +0100

    modify defi2.sol

--- defi_2/defi2.sol
+++ defi_2/defi2.sol
@@ -13,16 +13,22 @@ contract MarketIllustrator {
     
     constructor() public {
         administrateurs[msg.sender] = 1;
+        reputation[msg.sender] = 10;
     }
     
     function inscription(string memory nom) public{
         require(bannis[msg.sender] == 0, "Vous avez été bannis");
+        require(reputation[msg.sender] < 1, "Vous êtes déjà inscrits");
+        require(bytes(nom).length != 0, "Veuillez entrer un nom");
         illustrateurs[msg.sender] = nom;
         reputation[msg.sender] = 1;
     }
     
     function bannir(address illustrateur) public{
         require(administrateurs[msg.sender] == 1, "Vous n'êtes pas administrateur");
+        require(bannis[illustrateur] == 0, "L'illustrateur est déjà banni");
+        require(reputation[illustrateur] > 0, "L'illustrateur n'est pas inscrit");
+        delete illustrateurs[illustrateur];
         reputation[illustrateur] = 0;
         bannis[illustrateur] = 1;
     }
