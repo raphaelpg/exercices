085da3282eea65bf7037121dec1ca51724cb04ea
raphaelpg <raphael_pinto@outlook.com>
2019-11-22 19:33:56 +0100

    update dappHeader.jsx

---
+++ defi_2/client/src/components/dappHeader.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DappHeader extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Illustrator Market Place</h1>
+            </div>
+        );
+    }
+}
+
+export default DappHeader;
